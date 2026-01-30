98720eac6b658318ce7c7cc6b3beb6aef456f6df
Trixie <58610446+X8ETr1x@users.noreply.github.com>
2026-01-30 07:53:39 +0000

    Delete Compile.bat

--- Compile.bat
+++
@@ -1,9 +0,0 @@
-copy /v /y ".\scripting\1.10\FFCR\" ".\scripting\1.8\FFCR\"
-copy /v /y ".\scripting\1.10\forlix_floodcheck_redux.sp" ".\scripting\1.8\forlix_floodcheck_redux.sp"
-
-cd ./scripting/1.8
-spcomp.exe forlix_floodcheck_redux.sp -o../../plugins/forlix_floodcheck_redux_legacy.smx
-
-cd ../1.10
-spcomp.exe forlix_floodcheck_redux.sp -o../../plugins/forlix_floodcheck_redux.smx
-pause
